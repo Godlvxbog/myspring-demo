f4b6e3da9bf699bdddcd67e873182fb845b4aecf
xbog <1969084327@qq.com>
2019-08-01 01:01:11 +0800

    rmi测试

--- src/main/java/com/middleaware/rmi/client/rpc/ClientDemo.java
+++ src/main/java/com/middleaware/rmi/client/rpc/ClientDemo.java
@@ -2,6 +2,8 @@ package com.middleaware.rmi.client.rpc;
 
 import com.middleaware.rmi.server.IHelloService;
 
+import java.io.IOException;
+
 /**
  * 腾讯课堂搜索 咕泡学院
  * 加群获取视频：608583947
@@ -9,13 +11,14 @@ import com.middleaware.rmi.server.IHelloService;
  */
 public class ClientDemo {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         RpcClientProxy rpcClientProxy=new RpcClientProxy();
 
         IHelloService hello= null;
+
         try {
             hello = rpcClientProxy.clientProxy
-                    (IHelloService.class,"localhost",8888);
+                    (IHelloService.class,"127.0.0.1",1099);
             System.out.println(hello.sayHello("mic"));
         } catch (Exception e) {
             e.printStackTrace();
