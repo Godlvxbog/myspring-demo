242440f0f8bf3199411f0f535a15af36853a611d
xbog <1969084327@qq.com>
2019-08-05 22:45:34 +0800

    dubbo集群模式

--- src/main/java/com/middleaware/dubbo/server/provider/BootstrapCluster1.java
+++ src/main/java/com/middleaware/dubbo/server/provider/BootstrapCluster1.java
@@ -14,7 +14,7 @@ public class BootstrapCluster1 {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context=
                 new ClassPathXmlApplicationContext
-                        ("META-INF/spring/dubbo-cluster1.xml");
+                        ("dubbo/dubbo-cluster1.xml");
         context.start();
 
         System.in.read(); //阻塞当前进程
--- src/main/java/com/middleaware/dubbo/server/provider/BootstrapCluster2.java
+++ src/main/java/com/middleaware/dubbo/server/provider/BootstrapCluster2.java
@@ -14,7 +14,7 @@ public class BootstrapCluster2 {
     public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context=
                 new ClassPathXmlApplicationContext
-                        ("META-INF/spring/dubbo-cluster2.xml");
+                        ("dubbo/dubbo-cluster2.xml");
         context.start();
 
         System.in.read(); //阻塞当前进程
